1b1596d240c7edd2b7d1376106cb2379eb7ad1d5
oraby11 <159197144+oraby11@users.noreply.github.com>
2024-12-04 14:04:04 +0200

    Add files via upload

---
+++ MCAL/NVIC/NVIC_interface.h
@@ -0,0 +1,27 @@
+/**********************************************/
+/*********************************************/
+/**************** Author: Shaaban Oraby  ******/
+/*************** Layer: MCAL             ********/
+/**************  SWC: NVIC            **********/
+/**************  Version : 1.00         **********/
+
+#ifndef   NVIC_INTERFACE_H_
+#define   NVIC_INTERFACE_H_
+
+
+
+void NVIC_Enable_IRQ(u8 Copy_IRQn);
+
+void NVIC_Disable_IRQ(u8 Copy_IRQn);
+
+void NVIC_SetPending_IRQ(u8 Copy_IRQn);
+
+void NVIC_ClearPending_IRQ(u8 Copy_IRQn);
+
+u32 NVIC_GetPending_IRQ(u8 Copy_IRQn);
+
+void NVIC_SetPriority_IRQ(u8 Copy_IRQn,u32 Copy_Priority);
+u32 NVIC_GetPriority_IRQ(u8 Copy_IRQn);
+
+
+#endif
